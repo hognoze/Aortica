118bc44915414a4fe276d1403cbbeef67cf3c22d
hognoze <ellisw2204@gmail.com>
2025-11-28 01:51:26 +0000

    idk man

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 public class PlayerMovement : MonoBehaviour
 {
+
+    [Header("Movement")]
+    [SerializeField] float speed = 5;
+    // this is the same as an Unreal public variable!
+
     PlayerInput playerInput;
     InputAction pMove;
 
-    [SerializeField] float speed = 5;
-// this is the same as an Unreal public variable!
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         pMove = playerInput.actions.FindAction("Move");
-    }
 
+    }
 
     void Update()
     {
         MovePlayer();
     }
-// Every frame call the MovePlayer function
+
 
     void MovePlayer()
     {
         Vector2 direction = pMove.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+
     }
 
 }
